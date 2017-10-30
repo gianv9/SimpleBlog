896f1285bde031d44e44d3aa3185dc56dfeba2c4
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-10-30 18:37:24 -0400

    He hecho tantos cambios que ya no tengo ni idea.

--- public/index.php
+++ public/index.php
@@ -36,19 +36,19 @@ include_once '../config.php';
         define('BASE_URL', $baseUrl);
       #===============================================================================
 
-      #================================== VIEWS RENDERING =======================================
-          #by initializing the params parameter of the function we can call it using only the fileName.
-          # example:
-            #render('add_blog_post.php');
-        function render( $fileName , $params = [] ){
-          //skip output, strore everything internally
-          //everything in between will be returned as a string
-            ob_start();//Turn on output buffering
-            extract($params); //turn an associative array into a set of global variables
-            include_once $fileName;
-            return ob_get_clean();//Get current buffer contents and delete current output buffer
-        }
-        #===============================================================================
+    //   #================================== VIEWS RENDERING =======================================
+    //       #by initializing the params parameter of the function we can call it using only the fileName.
+    //       # example:
+    //         #render('add_blog_post.php');
+    //     function render( $fileName , $params = [] ){
+    //       //skip output, strore everything internally
+    //       //everything in between will be returned as a string
+    //         ob_start();//Turn on output buffering
+    //         extract($params); //turn an associative array into a set of global variables
+    //         include_once $fileName;
+    //         return ob_get_clean();//Get current buffer contents and delete current output buffer
+    //     }
+    //     #===============================================================================
 
 
 //
@@ -86,7 +86,17 @@ $router = new RouteCollector();
 
 ####
     $router->controller('admin/posts/insertPost', App\Controllers\Admin\PostsController::class);
+        
+        //error:
+        //Fatal error: Uncaught exception 'ReflectionException' with message 'Class App\Controllers\IndexController does not exist' in /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/vendor/phroute/phroute/src/Phroute/RouteCollector.php:315 Stack trace: #0 /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/vendor/phroute/phroute/src/Phroute/RouteCollector.php(315): ReflectionClass->__construct('App\\Controllers...') #1 /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/public/index.php(78): Phroute\Phroute\RouteCollector->controller('/', 'App\\Controllers...') #2 {main} thrown in /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/vendor/phroute/phroute/src/Phroute/RouteCollector.php on line 315
+        // ver -> http://developed.be/2014/08/29/composer-dump-autoload-laravel/
+        // possible solution: php composer.phar dump-autoload -o
 
+        // error:
+        // Catchable fatal error: Argument 1 passed to Twig_Filter::__construct() must be an instance of string, string given, called in /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/vendor/twig/twig/lib/Twig/Extension/Core.php on line 139 and defined in /opt/lampp/htdocs/cursoPhpPlatzi/ProyectoBlog/vendor/twig/twig/lib/Twig/Filter.php on line 35
+        // possible solution: set the environment to PHP 7..downloading a newer version of xampp
+        //  according to: https://stackoverflow.com/questions/41880071/argument-1-passed-to-twig-filter-construct-must-be-an-instance-of-string-s
+        // Verified in: https://twig.symfony.com/doc/2.x/intro.html
 #===========================#===============================#===============================#================================#
 
 //utilizamos el dispatcher
