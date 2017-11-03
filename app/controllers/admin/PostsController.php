e309a338a96b5da2456831e9081f3a0c4f1c5024
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-11-03 00:56:08 -0400

    Extraidos queries de los controladores, ahora asignados a eloquent/implemetados haciendo uso de eloquent

--- app/controllers/admin/PostsController.php
+++ app/controllers/admin/PostsController.php
@@ -2,7 +2,7 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
-
+use App\Models\BlogPosts;
 /**
  *
  */
@@ -11,20 +11,9 @@ class PostsController extends BaseController
 
   public function getIndex()
   {
-    # admin/posts or admin/posts/index
+   
+        $blog_posts = BlogPosts::all();
 
-          global $pdo;
-
-          // CODIGO PHP DE POSTS.PHP
-        // ======================
-        //creamos el query
-        $sql = "SELECT  * FROM blog_posts ORDER BY id DESC";//trae todos los blog posts empezando por el ultimo ID
-        //la preparamos y ejecutamos
-        $query = $pdo->prepare($sql);
-        $query->execute();
-        //hacemos el fetch de todas las filas
-        $blog_posts = $query->fetchAll(\PDO::FETCH_ASSOC);
-        // ======================
 
             // RENDER
           // ======================
@@ -39,21 +28,16 @@ class PostsController extends BaseController
           // ======================
   }
   public function postInsert(){
-    # admin/insert-posts
 
-        global $pdo;
+    $blogPost = new BlogPosts([
+      'title' => $_POST['title'],
+      'content' => $_POST['content']
+    ]);
+    $blogPost->save();
 
-    // CODIGO PHP DE INSERT-POST.PHP (POST)
-  // ======================
-        $sql = "INSERT INTO blog_posts (title, content) VALUES (:title, :content)";
-        //es buena practica preparar las sentencias con 'prepare' porque mejora el rendimiento de la aplicacion.
-        //ya que los queries quedan en cache para ser usados cuando yo quiera con 'execute'
-        $query = $pdo->prepare($sql);
-        $result = $query->execute([
-          'title' => $_POST['title'],
-          'content' => $_POST['content']
-        ]);
-  // ======================
+    $result = true;
+    
+
 
   // RENDER (POST)
   // ======================
--- app/models/BlogPost.php
+++ app/models/BlogPost.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlogPosts extends Model{
     protected $table = 'blog_posts';
+    protected $fillable = ['title', 'content'];//para que title y content se llenen automaticamente
 }
-
+//nota se crearon los registros created_at y updated_at que son atributos que eloquent llena por defeto
+//se puede desactivar pero nos hace falta
 ?>
