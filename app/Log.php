3c00ac10f53b98e9e8b16f8b479c5545d27a90a5
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-11-26 12:03:36 -0400

    Agregada clase Log que implementa el patro de diseno Singleton con monolog

---
+++ app/Log.php
@@ -0,0 +1,31 @@
+<?php
+namespace App;
+
+use Monolog\Logger;
+
+// clase que utiliza el patron de diseno Singleton
+class Log {
+    private static $_logger = null;
+
+    private static function getLogger(){
+        if(!isset(self::$_logger)){
+            // nombre del log: App Log
+            self::$_logger = new Logger('App Log');
+        }
+        return self::$_logger;
+    }
+
+    public static function logError($error){
+        // pushHandler: obten archivo en el que se va a guardar el error
+        // StreamHandler: porque se maneja un stream hacia un archivo
+        self::getLogger()->pushHandler(new StreamHandler('../logs/application.log', Logger::ERROR));
+        self::getLogger()->addError($error);
+    }
+
+    public static function logInfo($info){
+        // pushHandler: obten archivo en el que se va a guardar el Info
+        // StreamHandler: porque se maneja un stream hacia un archivo
+        self::getLogger()->pushHandler(new StreamHandler('../logs/application.log', Logger::INFO));
+        self::getLogger()->addInfo($info);
+    }
+}
