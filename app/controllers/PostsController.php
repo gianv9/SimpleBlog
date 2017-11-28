253eff6a0052cdd26292c9316911d30acfc1bf5d
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-11-28 11:08:01 -0400

    Agregado metodo index al contraldor de detalle de post

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -1,11 +1,24 @@
 <?php
+namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\BlogPosts;
 
 class PostsController extends BaseController
 {
-    public function getInfo($params){
-        $this->render('blog-posts.twig',[ 'params' => $params]);
+    //funcion que recibe el ID del blogpost
+    public function getIndex($arg = null){
+        //buscamos el blogpost con el id $arg
+        $blog_post = BlogPosts::where('id',$arg)->first();
+        //renderizamos el detalle del blogpost
+        return $this->render('blog-post.twig',
+            [ 
+                'blog_post' => $blog_post ,
+                'arg' => $arg
+            ]
+        );
     }
+
 }
+
+?>
